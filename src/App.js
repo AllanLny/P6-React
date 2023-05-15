f695ff642ec6d656fd7d3d156eaa88c7cf063e4b
Allan Lannoy <allan1411lannoy@gmail.com>
2023-05-15 16:53:10 +0200

    Résolution bug Banniere

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Home from "./components/Home/Home"
 import Page404 from './components/404/Page404';
 import About from './components/About/About';
@@ -7,11 +7,13 @@ import About from './components/About/About';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="P6-React/" element={<Home />} />
-        <Route path="P6-React/About" element={<About />} />
-        <Route path="P6-React/*" element={<Page404 />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="P6-React/" element={<Home />} />
+          <Route path="P6-React/About" element={<About />} />
+          <Route path="P6-React/*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
